874780c72f9e84fa0c4f0c60e356ca593e7ba2d6
Neil Hancock <hancocnd@gmail.com>
2021-05-29 17:17:04 -0400

    mock updates & bug fix

--- clients/location.py
+++ clients/location.py
@@ -6,9 +6,9 @@ from pushbullet import Pushbullet
 _LOGGER = logging.getLogger(__name__)
 
 class Location:
-    MOCK_STATUS = True
+    MOCK_NMAP = 'F0:AA:BB:CC:DD:EE'
 
-    def __init__(self, pushbullet_key, retry_count, ip_range, *phone_macs):
+    def __init__(self, pushbullet_key, retry_count, ip_range, phone_macs):
         self.PUSHBULLET_API_KEY = pushbullet_key
         self.RETRY_COUNT = retry_count
         self.ip_range = ip_range
@@ -16,20 +16,21 @@ class Location:
         self._last_check = True
 
     def get_nmap(self) -> str:
+        if 'macOS' in platform.platform():
+            return Location.MOCK_NMAP
+
         output = subprocess.run(["sudo", "nmap", "-sn", self.ip_range], capture_output=True)
         return output.stdout.decode('utf-8')
 
     def is_anyone_home(self, cached=False) -> bool:
         if cached:
             return self._last_check
-        if 'macOS' in platform.platform():
-            return Location.MOCK_STATUS
 
-        for i in range(Location.RETRY_COUNT):
+        for i in range(self.RETRY_COUNT):
             nmap_out = self.get_nmap()
             for mac in self.phone_macs:
                 if mac in nmap_out:
-                    if i < Location.RETRY_COUNT - 2:
+                    if i < self.RETRY_COUNT - 2:
                         _LOGGER.debug(f'Someone is home, took {i} iterations to check')
                     else:
                         _LOGGER.info(f'Someone is home, took {i} iterations to check')
